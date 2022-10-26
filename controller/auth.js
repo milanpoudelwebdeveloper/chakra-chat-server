fa2a0b4f4ea7844c0a42a9007b3d53da3dfce154
Milan Poudel <milanwebdeveloper1@gmail.com>
2022-10-26 16:08:22 +0545

    sign up controller checking existing username

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,7 @@
 const { loginSchema } = require("../schemas/loginSchema");
 const { validateForm } = require("./validateForm");
+const bcrypt = require("bcrypt");
+const pool = require("../db");
 
 exports.loginUser = async (req, res) => {
   validateForm(req, res);
@@ -8,5 +10,22 @@ exports.loginUser = async (req, res) => {
 
 exports.signUpUser = async (req, res) => {
   validateForm(req, res);
+  const { username, password } = req.body;
+  const existingUser = await pool.query(
+    "SELECT username from users WHERE username=$1",
+    [username]
+  );
+  if (existingUser.rowCount === 0) {
+    //hashing before saving
+    const hashedPass = await bcrypt.hash(password, 10);
+    const newUser = await pool.query(
+      "INSERT INTO users(username,passhash) VALUES($1,$2) RETURNING username",
+      [username, password]
+    );
+    //register
+    res.json({ loggedIn: true, username });
+  } else {
+    res.json({ loggedIn: false, message: "Username already exists" });
+  }
   res.status(200).json({ message: "sign up successful" });
 };
---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(28) NOT NULL UNIQUE,
+    passhash VARCHAR NOT NULL
+)
+
+
+INSERT INTO users(
+    username,
+    passhash
+) values ($1,$2);
